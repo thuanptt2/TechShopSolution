88719e8908d1d5369f3155aebc4a66d2f4fe490a
thuannef <liongentle@gmail.com>
2021-05-24 23:21:52 +0700

    Create and add template for AdminApp

---
+++ TechShopSolution.AdminApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TechShopSolution.AdminApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
