c2baa901c60a93ad257d33fe34ba88360bc97072
thuannef <liongentle@gmail.com>
2021-05-31 16:03:28 +0700

    Enable/Disable form edit and adjust some style

--- TechShopSolution.AdminApp/wwwroot/js/site.js
+++ TechShopSolution.AdminApp/wwwroot/js/site.js
@@ -12,5 +12,12 @@
 $(function () {
     $('#closeButtonModal').on('click', function () {
         $('#form-modal').modal('hide');
+    });
+    $('#btnEditInfo').on('click', function () {
+        if ($('#fieldSetUpdate').attr('disabled') == "disabled")
+            $('#fieldSetUpdate').removeAttr("disabled");
+        else $('#fieldSetUpdate').attr("disabled", "disabled");
+        $('#fieldSetUpdate :input:first').focus();
+
     })
-})
+});
