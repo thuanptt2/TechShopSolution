76ed6c487f3b694ce5d5e77f92757c4a6aecfe89
Thanh Thuận <thuanneuwu@gmail.com>
2021-06-20 00:51:38 +0700

    Load Multi Level Category - Index View (App)

---
+++ TechShopSolution.WebApp/Controllers/Components/SlideBarViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechShopSolution.ApiIntegration;
+using TechShopSolution.ViewModels.Catalog.Category;
+
+namespace TechShopSolution.WebApp.Controllers.Components
+{
+    public class SlideBarViewComponent : ViewComponent
+
+    {
+        private readonly ICategoryApiClient _categorytApiClient;
+
+        public SlideBarViewComponent(ICategoryApiClient categorytApiClient)
+        { 
+            _categorytApiClient = categorytApiClient;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _categorytApiClient.GetAllCategory();
+            return View(items);
+        }
+
+    }
+}
