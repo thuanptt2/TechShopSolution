c579d6d1b0d6d529d3910ceef1fe48b5e70fec22
Thanh Thuận <thuanneuwu@gmail.com>
2021-06-24 14:12:33 +0700

    Add Cart View

--- TechShopSolution.WebApp/wwwroot/js/Cart.js
+++ TechShopSolution.WebApp/wwwroot/js/Cart.js
@@ -1,1 +1,14 @@
-﻿
+﻿var CartController = function () {
+    this.initialize = function () {
+
+    }
+    function loadData() {
+         $.ajax({
+            type: "POST",
+            url: '/Cart/GetListItem/',
+            success: function (res) {
+                console.log(res);
+            },
+        })
+    }
+}
