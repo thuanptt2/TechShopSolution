e0dbc0098afc601271276eee80fe3ba36328c60e
Thanh Thuận <thuanneuwu@gmail.com>
2021-06-30 17:35:28 +0700

    Create Coupon Api Client

---
+++ TechShopSolution.ApiIntegration/ICouponApiClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TechShopSolution.ViewModels.Catalog.Coupon;
+using TechShopSolution.ViewModels.Common;
+
+namespace TechShopSolution.ApiIntegration
+{
+    public interface ICouponApiClient
+    {
+        Task<PagedResult<CouponViewModel>> GetCouponPagings(GetCouponPagingRequest request);
+        Task<ApiResult<bool>> CreateCoupon(CouponCreateRequest request);
+        Task<ApiResult<bool>> UpdateCoupon(CouponUpdateRequest request);
+        Task<ApiResult<bool>> ChangeStatus(int Id);
+        Task<ApiResult<bool>> Delete(int cusID);
+        Task<ApiResult<CouponViewModel>> GetById(int id);
+        Task<bool> isValidSlug(int id, string code);
+    }
+}
