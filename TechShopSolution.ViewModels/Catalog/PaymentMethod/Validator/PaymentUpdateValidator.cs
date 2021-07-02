3651aac44e97348662a845c21d404aa32ddb3fd1
Thanh Thuận <thuanneuwu@gmail.com>
2021-07-02 23:15:46 +0700

    Validate Payment Method Functions form

---
+++ TechShopSolution.ViewModels/Catalog/PaymentMethod/Validator/PaymentUpdateValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechShopSolution.ViewModels.Catalog.PaymentMethod.Validator
+{
+    public class PaymentUpdateValidator : AbstractValidator<PaymentUpdateRequest>
+    {
+        public PaymentUpdateValidator()
+        {
+            RuleFor(x => x.name).NotEmpty().WithMessage("Tên phương thức không được để trống")
+                                .MaximumLength(255).WithMessage("Tên thương hiệu không thể vượt quá 255 kí tự");
+        }
+    }
+}
