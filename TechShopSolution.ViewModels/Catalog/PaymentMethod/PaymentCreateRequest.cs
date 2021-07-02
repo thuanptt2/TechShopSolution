ac177f195f5ce46038d4060fee9cca190cb01208
Thanh Thuận <thuanneuwu@gmail.com>
2021-07-02 21:35:21 +0700

    Create PaymentMethod ViewModels

---
+++ TechShopSolution.ViewModels/Catalog/PaymentMethod/PaymentCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechShopSolution.ViewModels.Catalog.PaymentMethod
+{
+    public class PaymentCreateRequest
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ TechShopSolution.ViewModels/Catalog/PaymentMethod/PaymentUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechShopSolution.ViewModels.Catalog.PaymentMethod
+{
+    public class PaymentUpdateRequest
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public bool isActive { get; set; }
+    }
+}
