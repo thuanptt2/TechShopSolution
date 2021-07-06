38ac980360ccd256ecde7904f9757457417dade0
Thanh Thuận <thuanneuwu@gmail.com>
2021-07-06 17:37:05 +0700

    Add new two column in Transporter table

---
+++ TechShopSolution.Data/Migrations/20210706103458_UpdateTableTransporter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TechShopSolution.Data.Migrations
+{
+    public partial class UpdateTableTransporter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "image",
+                table: "Transporter",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "link",
+                table: "Transporter",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "image",
+                table: "Transporter");
+
+            migrationBuilder.DropColumn(
+                name: "link",
+                table: "Transporter");
+        }
+    }
+}
