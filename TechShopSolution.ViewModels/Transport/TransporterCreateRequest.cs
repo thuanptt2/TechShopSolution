c9367398e692885ad5437a9a467b2300e6d6d206
Thanh Thuận <thuanneuwu@gmail.com>
2021-07-07 06:08:58 +0700

    Insert Data Transporter And Display form field name

--- TechShopSolution.ViewModels/Transport/TransporterCreateRequest.cs
+++ TechShopSolution.ViewModels/Transport/TransporterCreateRequest.cs
@@ -1,15 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TechShopSolution.ViewModels.Transport
 {
     public class TransporterCreateRequest
     {
+        [Display(Name = "Tên đơn vị")]
         public string name { get; set; }
+        [Display(Name = "Hình ảnh")]
         public IFormFile image { get; set; }
+        [Display(Name = "Link tra cứu")]
         public string link { get; set; }
+        [Display(Name = "Trạng thái")]
         public bool isActive { get; set; }
     }
 }
