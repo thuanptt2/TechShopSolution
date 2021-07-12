b93fda1702333f0928a96a3be624db6f0cb024ad
Thanh Thuận <thuanneuwu@gmail.com>
2021-07-12 15:46:42 +0700

    Create Slide View Models

---
+++ TechShopSolution.ViewModels/Website/SlideCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechShopSolution.ViewModels.Website
+{
+    public class SlideCreateRequest
+    {
+        public string image { get; set; }
+        public int display_order { get; set; }
+        public string link { get; set; }
+        public bool status { get; set; }
+    }
+}
---
+++ TechShopSolution.ViewModels/Website/SlideUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechShopSolution.ViewModels.Website
+{
+    public class SlideUpdateRequest
+    {
+        public int id { get; set; }
+        public string image { get; set; }
+        public int display_order { get; set; }
+        public string link { get; set; }
+        public bool status { get; set; }
+    }
+}
