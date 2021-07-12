1001b6cbd56304ac2eb4c7b727bd22a87c283bc7
Thanh Thuận <thuanneuwu@gmail.com>
2021-07-13 02:00:38 +0700

    Create Contact View Model & Request Models

---
+++ TechShopSolution.ViewModels/Website/Contact/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechShopSolution.ViewModels.Website.Contact
+{
+    public class ContactViewModel
+    {
+        public int id { get; set; }
+        public string company_name { get; set; }
+        public string adress { get; set; }
+        public string phone { get; set; }
+        public string hotline { get; set; }
+        public string company_logo { get; set; }
+        public string fax { get; set; }
+        public string email { get; set; }
+        public string social_fb { get; set; }
+        public string social_instagram { get; set; }
+        public string social_youtube { get; set; }
+        public string social_twitter { get; set; }
+    }
+}
