91c93348ad8a81d27581a2e84494dc760f52e494
Thanh Thuận <thuanneuwu@gmail.com>
2021-07-14 00:47:34 +0700

    Create Navbar Top Menu Component

--- TechShopSolution.WebApp/Models/HomeViewModel.cs
+++ TechShopSolution.WebApp/Models/HomeViewModel.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TechShopSolution.ViewModels.Catalog.Product;
+using TechShopSolution.ViewModels.Website.Contact;
 using TechShopSolution.ViewModels.Website.Slide;
 
 namespace TechShopSolution.WebApp.Models
 {
     public class HomeViewModel
     {
-
         public List<SlideViewModel> Sliders { get; set; }
         public PublicProductsViewModel FeaturedProducts { get; set; }
         public PublicProductsViewModel BestSellerProducts { get; set; }
